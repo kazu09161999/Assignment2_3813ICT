b7237ea048b558b4343b75e27e421fe3aa510d84
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-10-06 20:40:15 +1000

    message can be sent through socket

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Assignment2_3813ICT
 
 # yoneyamakazuki@north-10-21-13-77 Assignment2_3813ICT % npm run dev
+
--- server.js
+++ server.js
@@ -12,18 +12,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
-  // Welcome current user
+  console.log('New Connection');
+// Welcome current user
   socket.emit('message', 'Welcome to This Application');
 
-  // Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');
+//   // Broadcast when a user connects
+//   socket.broadcast.emit('message', 'A user has joined the chat');
 
-  // Runs when clients disconnects
-  socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
-  });
+//   // Runs when clients disconnects
+//   socket.on('disconnect', () => {
+//     io.emit('message', 'A user has left the chat');
 });
+
+//   // Listen for chatMessage
+//   socket.on('chatMessage', msg => {
+//     io.emit('message', msg);
+//   });
+// });
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
