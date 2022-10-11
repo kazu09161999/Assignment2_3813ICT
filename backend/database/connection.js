3465082c198267c0243af83fed901db3b21288f7
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-10-12 03:44:11 +1000

    delete some

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -6,8 +6,6 @@ const connectDB = async() => {
     const con= await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser:true,
       useUnifiedTopology:true
-      // useFindAndModify:false,
-      // useCreateIndex:true
     })
 
     console.log(`MongoDB connected: ${con.connection.host}`);
