585518cf138f1f4977f07a8cee3f81e343be62cf
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-10-12 20:31:20 +1000

    change name

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ io.on('connection', socket => {
 
     socket.join(user.room);
     // Welcome current user
-    socket.emit('message', formatMessage(botName,'Welcome to This Application'));
+    socket.emit('message', formatMessage(botName,'Welcome to Chat Application'));
 
     // Broadcast when a user connects
     socket.broadcast
